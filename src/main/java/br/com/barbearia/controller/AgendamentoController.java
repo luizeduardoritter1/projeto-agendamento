fe1d5795484869183e2bbfc7c733dcc39308df68
Luiz Eduardo Ritter Leal <luizeduardoritter6@gmail.com>
2024-06-18 08:44:34 -0300

    create controller Agendamento

---
+++ src/main/java/br/com/barbearia/controller/AgendamentoController.java
@@ -0,0 +1,10 @@
+package br.com.barbearia.controller;
+
+import br.com.barbearia.model.Agendamento;
+
+import java.util.ArrayList;
+
+public class AgendamentoController {
+// Aqui estou criando uma lista para agendamento
+    private ArrayList<Agendamento> agendameto = new ArrayList<>();
+}
